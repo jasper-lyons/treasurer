f3fcd12748ef5db5b5849902eb7d4a1e0d33bffd
Jasper Lyons <jasper.lyons@gmail.com>
2021-11-07 15:28:21 +0000

    Draws spectrogram to canvas

---
+++ worker.js
@@ -0,0 +1,27 @@
+let canvas, context, fft = [], duration, position
+
+self.onmessage = function (event) {
+  if (event.data.canvas) {
+    canvas = event.data.canvas
+    context = canvas.getContext('2d')
+    self.postMessage('fft')
+  } else if (event.data.fft) {
+    fft = event.data.fft
+    duration = event.data.duration
+    position = event.data.position
+    if (duration && position)
+      draw()
+    self.postMessage('fft')
+  }
+}
+
+function draw() {
+  for (let i = 0; i < fft.length; i++) {
+    let amplitude = fft[i]
+    let x = (position / duration) * canvas.width
+    let y = canvas.height - ((i / fft.length) * canvas.height)
+    context.strokeStyle = `rgb(${amplitude}, ${amplitude}, ${amplitude})`
+    context.fillStyle = `rgb(${amplitude}, ${amplitude}, ${amplitude})`
+    context.fillRect(x, y, canvas.width / duration, canvas.height / fft.length)
+  }
+}
